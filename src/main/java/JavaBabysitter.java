83d9d33bfa5299bf792dcd76419ae236098be5f7
Brandon Cromer <bcromer@pillartechnology.com>
2015-06-22 22:56:29 -0400

    Added test for 1 hour from start to end, after bed before midnight

--- src/main/java/JavaBabysitter.java
+++ src/main/java/JavaBabysitter.java
@@ -1,8 +1,17 @@
 public class JavaBabysitter {
 
     private static int START_TO_BED_PAY = 12;
+    private static int BED_TO_MIDNIGHT_PAY = 8;
 
     public static int calculatePay (int start, int bed, int end) {
-        return (end - start) * START_TO_BED_PAY;
+        int pay = 0;
+
+        if (bed > start) {
+            pay = (end - start) * START_TO_BED_PAY;
+        } else {
+            pay = BED_TO_MIDNIGHT_PAY;
+        }
+
+        return pay;
     }
 }
--- src/test/java/JavaBabysitterTest.java
+++ src/test/java/JavaBabysitterTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 public class JavaBabysitterTest {
 
     private int START_TO_BED_PAY = 12;
+    private int BED_TO_MIDNIGHT_PAY = 8;
 
     @Test
     public void shouldCalculatePayForOneHourBetweenStartAndEndBeforeBed() {
@@ -16,5 +17,11 @@ public class JavaBabysitterTest {
     public void shouldCalculatePayForMultipleHoursBetweenStartAndEndBeforeBed() {
         int expectedPay = START_TO_BED_PAY * 2;
         assertEquals(expectedPay, JavaBabysitter.calculatePay(5, 7, 7));
+    }
+
+    @Test
+    public void shouldCalculatePayForOneHourBetweenStartAndEndAfterBedBeforeMidnight() {
+        int expectedPay = BED_TO_MIDNIGHT_PAY;
+        assertEquals(expectedPay, JavaBabysitter.calculatePay(5, 5, 6));
     }
 }
